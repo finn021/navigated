1a67ec99e650c0c18bed681fd616bb9f603956a7
Finn <79959229+Finn021@users.noreply.github.com>
2021-08-30 06:14:19 +1000

    Update us_states_quiz.js

--- assets/js/us_states_quiz.js
+++ assets/js/us_states_quiz.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
     $('#map').usmap({});
   });
+
+$('#map').usmap({
+  stateStyles: {fill: '#000000'},
+  stateHoverStyles: {fill: '#121212'},
+  showLabels: false
+});
