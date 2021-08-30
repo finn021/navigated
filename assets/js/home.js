aa3975b37ecf45528e9bbeb0df4f56a5ba158d1d
Finn <79959229+Finn021@users.noreply.github.com>
2021-08-30 20:00:52 +1000

    Update home.js

--- assets/js/home.js
+++ assets/js/home.js
@@ -5,3 +5,22 @@ $('.card').sort(function(a, b) {
     return 1;
   }
 }).appendTo('.card-container');
+
+function searchAll() {
+  var input, filter, container, card, a, i, txtValue;
+  input = document.querySelector(".search-box");
+  filter = input.value.toUpperCase();
+  container = document.querySelector(".card-container");
+  card = document.querySelector(".card");
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < card.length; i++) {
+    h3 = card[i].getElementsByTagName("h3")[0];
+    txtValue = h3.textContent || h3.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      card[i].style.display = "";
+    } else {
+      card[i].style.display = "none";
+    }
+  }
+}
