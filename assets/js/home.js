47cef123e33b6d0ce4029659a413d8bad3a43f65
Finn <79959229+Finn021@users.noreply.github.com>
2021-08-30 20:14:39 +1000

    Update home.js

--- assets/js/home.js
+++ assets/js/home.js
@@ -8,8 +8,9 @@ document.querySelector(".search-box").addEventListener("input", function (e) {
   for (i = 0; i < card.length; i++) {
     h3 = card[i].getElementsByTagName("h3")[0];
     txtValue = h3.textContent || h3.innerText;
+    alert(input.value);
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      card[i].style.display = "";
+      card[i].style.display = "flex";
     } else {
       card[i].style.display = "none";
     }
