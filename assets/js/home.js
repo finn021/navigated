1c8e48e17739ff6929b438e6a91769219701130f
Finn <79959229+Finn021@users.noreply.github.com>
2021-08-30 20:07:22 +1000

    Update home.js

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,19 +1,10 @@
-$('.card').sort(function(a, b) {
-  if (a.textContent < b.textContent) {
-    return -1;
-  } else {
-    return 1;
-  }
-}).appendTo('.card-container');
-
-function searchAll() {
+document.querySelector(".search-box").addEventListener("input", function (e) {
   var input, filter, container, card, a, i, txtValue;
   input = document.querySelector(".search-box");
   filter = input.value.toUpperCase();
   container = document.querySelector(".card-container");
   card = document.querySelector(".card");
-
-  // Loop through all list items, and hide those who don't match the search query
+  
   for (i = 0; i < card.length; i++) {
     h3 = card[i].getElementsByTagName("h3")[0];
     txtValue = h3.textContent || h3.innerText;
@@ -23,4 +14,4 @@ function searchAll() {
       card[i].style.display = "none";
     }
   }
-}
+});
