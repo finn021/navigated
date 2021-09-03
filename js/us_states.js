e105e4d835204701c3e1b97adb873888811113ab
Finn <79959229+Finn021@users.noreply.github.com>
2021-09-03 23:56:56 +1000

    Update us_states.js

--- js/us_states.js
+++ js/us_states.js
@@ -1,131 +1,4 @@
-let stateCodes = {
-  alabama: "al",
-  alaska: "ak",
-  americansamoa: "as",
-  arizona: "az",
-  arkansas: "ar",
-  california: "ca",
-  colorado: "co",
-  connecticut: "ct",
-  delaware: "de",
-  districtofcolumbia: "dc",
-  federatedstatesofmicronesia: "fm",
-  florida: "fl",
-  georgia: "ga",
-  guam: "gu",
-  hawaii: "hi",
-  idaho: "id",
-  illinois: "il",
-  indiana: "in",
-  iowa: "ia",
-  kansas: "ks",
-  kentucky: "ky",
-  louisiana: "la",
-  maine: "me",
-  marshallislands: "mh",
-  maryland: "md",
-  massachusetts: "ma",
-  michigan: "mi",
-  minnesota: "mn",
-  mississippi: "ms",
-  missouri: "mo",
-  montana: "mt",
-  nebraska: "ne",
-  nevada: "nv",
-  newhampshire: "nh",
-  newjersey: "nj",
-  newmexico: "nm",
-  newyork: "ny",
-  northcarolina: "nc",
-  northdakota: "nd",
-  northernmarianaislands: "mp",
-  ohio: "oh",
-  oklahoma: "ok",
-  oregon: "or",
-  palau: "pw",
-  pennsylvania: "pa",
-  puertorico: "pr",
-  rhodeisland: "ri",
-  southcarolina: "sc",
-  southdakota: "sd",
-  tennessee: "tn",
-  texas: "tx",
-  utah: "ut",
-  vermont: "vt",
-  virginislands: "vi",
-  virginia: "va",
-  washington: "wa",
-  westvirginia: "wv",
-  wisconsin: "wi",
-  wyoming: "wy",
-};
-
-var states = [
-  "alabama",
-  "alaska",
-  "americansamoa",
-  "arizona",
-  "arkansas",
-  "california",
-  "colorado",
-  "connecticut",
-  "delaware",
-  "districtofcolumbia",
-  "federatedstatesofmicronesia",
-  "florida",
-  "georgia",
-  "guam",
-  "hawaii",
-  "idaho",
-  "illinois",
-  "indiana",
-  "iowa",
-  "kansas",
-  "kentucky",
-  "louisiana",
-  "maine",
-  "marshall islands",
-  "maryland",
-  "massachusetts",
-  "michigan",
-  "minnesota",
-  "mississippi",
-  "missouri",
-  "montana",
-  "nebraska",
-  "nevada",
-  "newhampshire",
-  "newjersey",
-  "newmexico",
-  "newyork",
-  "northcarolina",
-  "northdakota",
-  "northernmarianaislands",
-  "ohio",
-  "oklahoma",
-  "oregon",
-  "palau",
-  "pennsylvania",
-  "puertorico",
-  "rhodeisland",
-  "southcarolina",
-  "southdakota",
-  "tennessee",
-  "texas",
-  "utah",
-  "vermont",
-  "virginisland",
-  "virginia",
-  "washington",
-  "westvirginia",
-  "wisconsin",
-  "wyoming",
-];
-
-window.onbeforeunload = function() {
-  return "If you leave this page, your progress will not be saved. Are you sure you want to continue?";
-};
-
+const states = ["alabama","alaska","arizona","arkansas","california","colorado","connecticut","delaware","florida","georgia","hawaii","idaho","illinois","indiana","iowa","kansas","kentucky","louisiana","maine","maryland","massachusetts","michigan","minnesota","mississippi","missouri","montana","nebraska","nevada","newhampshire","newjersey","newmexico","newyork","northcarolina","northdakota","ohio","oklahoma","oregon","pennsylvania","rhodeisland","southcarolina","southdakota","tennessee","texas","utah","vermont","virginia","washington","westvirginia","wisconsin","wyoming"];
 var stateValues = new Array(50).fill(false);
 
 $(answerInput).on("input", function (e) {
@@ -136,30 +9,13 @@ $(answerInput).on("input", function (e) {
       var index = states.indexOf(formattedInput);
       if (stateValues[index] == false) {
         stateValues[index] = true;
+        console.log(stateValues);
         $("#answerInput").val("");
-        updateAll();
+        var count = stateValues.reduce(function(n, val) {
+        return n + (val === true); }, 0);
+         $("#count").text(count + "/50");
+        console.log(count);
       }
     }
   }
 });
-
-function updateAll {
-  var indexes = getAllIndexes(stateValues, true);
-  for (let i = 0; i < indexes.length; i++) {
-    indexes //continue
-  
-}
-  $('#map').usmap({
-  stateSpecificStyles: {
-    'MD': {fill: 'yellow'},
-    'VA': {fill: 'teal'}
-  } 
-});
-
-function getAllIndexes(arr, val) {
-    var indexes = [], i;
-    for(i = 0; i < arr.length; i++)
-        if (arr[i] === val)
-            indexes.push(i);
-    return indexes;
-}
