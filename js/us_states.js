4ee6d18524664ae7a7ffa19367a6731ff2293557
Finn <79959229+Finn021@users.noreply.github.com>
2021-09-04 15:48:36 +1000

    Update us_states.js

--- js/us_states.js
+++ js/us_states.js
@@ -1,4 +1,22 @@
-const states = ["alabama","alaska","arizona","arkansas","california","colorado","connecticut","delaware","florida","georgia","hawaii","idaho","illinois","indiana","iowa","kansas","kentucky","louisiana","maine","maryland","massachusetts","michigan","minnesota","mississippi","missouri","montana","nebraska","nevada","newhampshire","newjersey","newmexico","newyork","northcarolina","northdakota","ohio","oklahoma","oregon","pennsylvania","rhodeisland","southcarolina","southdakota","tennessee","texas","utah","vermont","virginia","washington","westvirginia","wisconsin","wyoming"];
+const states = [
+  'alabama','alaska','arizona','arkansas','california','colorado','connecticut',
+  'delaware','florida','georgia','hawaii','idaho','illinois','indiana','iowa','kansas',
+  'kentucky','louisiana','maine','maryland','massachusetts','michigan','minnesota',
+  'mississippi','missouri','montana','nebraska','nevada','newhampshire','newjersey',
+  'newmexico','newyork','northcarolina','northdakota','ohio','oklahoma','oregon',
+  'pennsylvania','rhodeisland','southcarolina','southdakota','tennessee','texas','utah',
+  'vermont','virginia','washington','westvirginia','wisconsin','wyoming'
+];
+const statesFormatted = [
+  'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut',
+  'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas',
+  'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 
+  'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 
+  'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon',
+  'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 
+  'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+];
+
 var stateValues = new Array(50).fill(false);
 
 var body = document.body;
@@ -13,7 +31,7 @@ for (var i = 0; i < 50; i++) {
         break;
        } else {
          var td = tr.insertCell();
-         td.appendChild(document.createTextNode(states[i]));
+         td.appendChild(document.createTextNode(statesFormatted[i]));
          if (i == 1 && j == 1) {
            td.setAttribute('rowSpan', '2');
          }
